80012223b2e3931bd9493af95a28bdfc8d0f0f57
Anastasia Talalaeva <LightTross@yandex.ru>
2023-05-10 16:53:36 +0300

    Исправление ошибок по тестам

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,17 +3,20 @@ const jwt = require('jsonwebtoken');
 const { NODE_ENV, JWT_SECRET } = process.env;
 const { UnauthorizedError } = require('../errors/UnauthorizedError');
 
+const getBearerToken = (header) => header.replace('Bearer ', '').replace('jwt=', '');
+
 module.exports = (req, res, next) => {
   // достаём авторизационный заголовок
   const { authorization } = req.headers;
 
   // убеждаемся, что он есть или начинается с Bearer
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!authorization || (!authorization.startsWith('Bearer ') || !authorization.startsWith('jwt='))) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   // извлекаем токен
-  const token = authorization.replace('Bearer ', '');
+  const token = getBearerToken(authorization);
+
   let payload;
 
   try {
--- middlewares/validations.js
+++ middlewares/validations.js
@@ -13,7 +13,7 @@ const signUpValidation = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8).max(24),
-    name: Joi.string().required().min(2).max(30),
+    name: Joi.string(),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(/^(ftp|http|https):\/\/[^ "]+$/).message('Неверный url адрес'),
   }),
